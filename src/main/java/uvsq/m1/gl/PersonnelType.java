7ae26d8ab33889528b59d0905c6de62cd4a89620
myfirstOne <meryem.elmchichi@gmail.com>
2020-04-27 02:26:33 +0200

    commit

---
+++ src/main/java/uvsq/m1/gl/PersonnelType.java
@@ -0,0 +1,12 @@
+package uvsq.m1.gl;
+
+import java.io.Serializable;
+
+public abstract class PersonnelType implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	abstract public boolean isGroupe();
+
+}
